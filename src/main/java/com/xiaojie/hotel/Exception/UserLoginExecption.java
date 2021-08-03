62a5ddc2e8ea3a10a7602ee47ef42bb3cdbf1fd2
1933823757 <1933823757@qq.com>
2021-08-03 17:33:30 +0800

    更新配置文件

---
+++ src/main/java/com/xiaojie/hotel/Exception/UserLoginExecption.java
@@ -0,0 +1,10 @@
+package com.xiaojie.hotel.Exception;
+
+public class UserLoginExecption extends Exception {
+    public UserLoginExecption() {
+    }
+
+    public UserLoginExecption(String message) {
+        super(message);
+    }
+}
